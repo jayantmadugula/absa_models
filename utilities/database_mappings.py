ea610cd7e9a3d57ebbdd5af529c02a1a96592cdc
Jayant Madugula <jayant.madugula@gmail.com>
2022-05-16 22:30:38 -0700

    Added expected mappings for tables in db

--- utilities/database_mappings.py
+++ utilities/database_mappings.py
@@ -10,6 +10,45 @@ def get_table_name(
     get_documents: bool = True, 
     get_metadata: bool = False,
     ngram_len: int = None) -> str:
+
+    def construct_restaurantreviews_ngram_table_name(ngram_len: int, *args) -> str:
+        base_name = f'restaurantreviews_n={ngram_len}'
+        if len(args) == 0: return base_name
+        else: return '_'.join([base_name] + list(args))
+
+    def construct_restaurantreviews_document_table_name() -> str:
+        return 'restaurantreviews_reviews'
+
+    def construct_restaurantreviews_restaurants_table_name() -> str:
+        return 'restaurantreviews_restaurants'
+
+    def construct_semeval16_ngram_table_name(ngram_len: int, *args) -> str:
+        base_name = f'semeval16={ngram_len}'
+        if len(args) == 0: return base_name
+        else: return '_'.join([base_name] + list(args))
+
+    def construct_semeval16_document_table_name() -> str:
+        return 'semeval16_reviews'
+
+    def construct_semeval16_metadata_table_name() -> str:
+        return 'semeval16_opinion_data'
+
+    def construct_socc_ngram_table_name(ngram_len: int, *args) -> str:
+        base_name = f'socc={ngram_len}'
+        if len(args) == 0: return base_name
+        else: return '_'.join([base_name] + list(args))
+
+    def construct_socc_document_table_name() -> str:
+        return 'socc_articles'
+
+    def construct_sst_ngram_table_name(ngram_len: int, *args) -> str:
+        base_name = f'sst={ngram_len}'
+        if len(args) == 0: return base_name
+        else: return '_'.join([base_name] + list(args))
+
+    def construct_sst_document_table_name() -> str:
+        return 'sst_phrases'
+    
     if dataset == 'restaurantreviews':
         if get_documents:
             return construct_restaurantreviews_document_table_name()
@@ -45,40 +84,14 @@ def get_table_name(
     else:
         raise ValueError('Invalid dataset option.')
 
-def construct_restaurantreviews_ngram_table_name(ngram_len: int, *args) -> str:
-    base_name = f'restaurantreviews_n={ngram_len}'
-    if len(args) == 0: return base_name
-    else: return '_'.join([base_name] + list(args))
 
-def construct_restaurantreviews_document_table_name() -> str:
-    return 'restaurantreviews_reviews'
-
-def construct_restaurantreviews_restaurants_table_name() -> str:
-    return 'restaurantreviews_restaurants'
-
-def construct_semeval16_ngram_table_name(ngram_len: int, *args) -> str:
-    base_name = f'semeval16={ngram_len}'
-    if len(args) == 0: return base_name
-    else: return '_'.join([base_name] + list(args))
-
-def construct_semeval16_document_table_name() -> str:
-    return 'semeval16_reviews'
-
-def construct_semeval16_metadata_table_name() -> str:
-    return 'semeval16_opinion_data'
-
-def construct_socc_ngram_table_name(ngram_len: int, *args) -> str:
-    base_name = f'socc={ngram_len}'
-    if len(args) == 0: return base_name
-    else: return '_'.join([base_name] + list(args))
-
-def construct_socc_document_table_name() -> str:
-    return 'socc_articles'
-
-def construct_sst_ngram_table_name(ngram_len: int, *args) -> str:
-    base_name = f'sst={ngram_len}'
-    if len(args) == 0: return base_name
-    else: return '_'.join([base_name] + list(args))
-
-def construct_sst_document_table_name() -> str:
-    return 'sst_phrases'
+def get_text_column_name(table_name: str):
+    ''' Returns the primary text column for each known table. '''
+    if table_name in ['restaurantreviews_reviews', 'semeval16_reviews']:
+        return 'review'
+    elif table_name == 'socc_articles':
+        return 'article_text'
+    elif table_name == 'sst_phrases':
+        return 'phrase'
+    elif '=' in table_name:
+        return 'ngram'
