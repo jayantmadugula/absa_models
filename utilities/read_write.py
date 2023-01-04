df774cc2931e89365876e756b1d02ea36f0bc2a7
Jayant Madugula <jayant.madugula@gmail.com>
2023-01-03 21:00:03 -0800

    Documentation updates

--- utilities/read_write.py
+++ utilities/read_write.py
@@ -10,12 +10,20 @@ import numpy as np
 
 
 def save_embeddings(
-    embeddings_list: Iterable[str], 
+    embeddings_list: Iterable[np.ndarray], 
     ids_list: Iterable[int], 
     directory_path: str,
     dry_run: bool = False):
     '''
     Saves an embedding matrix to an .npy file.
+
+    Parameters:
+    - `embeddings_list`: iterable of embeddings (1-dimensional arrays of floats)
+    - `ids_list`: iterable of unique ids for each element in `embeddings_list`, with the same ordering
+    - `directory_path`: path to the directory where the embeddings will be saved
+    - `dry_run`: if True, no embeddings will actually be saved, but additional logging is provided
+
+    `embeddings_list` and `ids_list` must have the same number of elements and ordering (i.e. `ids_list`[2] must be the unique id for `embeddings_list[2]`).
     '''
     if dry_run:
         print('[Debug] Save function received a list of embeddings' \
