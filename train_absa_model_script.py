56aaaebf8efd5e4403919afc89192e4df52f44db
Jayant Madugula <jayantmadugula@gmail.com>
2023-01-30 23:02:27 -0800

    Added cli arguments for certain training parameters

--- train_absa_model_script.py
+++ train_absa_model_script.py
@@ -2,52 +2,117 @@
 TEMP: The contents of this script should be moved to classes. Do NOT have a script for every single model...
 '''
 
+from datetime import datetime
+import json
 import os
 from data_handling.embedding_generation import generate_ngram_matrix
 from data_handling import data_loaders, data_generators
 from models.abae_models import SimpleABAE
 import numpy as np
 import argparse
+from enum import Enum
 
-def setup_argparse():
+class SupportedAspectModels(Enum):
+    SIMPLE_ABAE = 'SimpleABAE'
+
+class SupportedDatasets(Enum):
+    RESTAURANT_REVIEWS = 'restaurant_reviews'
+
+def setup_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='''
         Trains an aspect-detection model based on an auto-encoder architecture.
 
         This script supports a number of different models. For more information, run $ python train_absa_model_script --help
         ''')
     
-    parser.add_argument('')
+    parser.add_argument(
+        'dataset_name',
+        type=str,
+        choices=[e.value for e in SupportedDatasets],
+        help='Name of the dataset used for trainng.'
+    )
+
+    parser.add_argument(
+        'model',
+        type=str,
+        choices=[e.value for e in SupportedAspectModels],
+        help='Type of model to train.'
+    )
+
+    parser.add_argument(
+        '-n',
+        '--ngram_size',
+        type=int,
+        required=True,
+        help='Size of the ngrams the model is training on. For the aspect models defined in this repository, this number must be odd.'
+    )
+
+    parser.add_argument(
+        '-e',
+        '--epochs',
+        type=int,
+        required=True,
+        help='The number of epochs to train the model. Each epoch trains the full network on every element in the dataset.'
+    )
+
+    parser.add_argument(
+        '-a',
+        '--number_of_aspects',
+        type=int,
+        default=14,
+        required=False,
+        help='The number of "classes", or aspects, in the model\'s output. This corresponds to the shape of the model\'s output layer.'
+    )
+
+
+    return parser
+
+def validate_arguments(args):
+    ''' Additional validation for input arguments. '''
+    if args.ngram_size % 2 != 0:
+        raise ValueError('ngram_size must be an odd number')
 
 if __name__ == '__main__':
     print('Starting the train_absa_model_script.')
 
-    # Set constants.
-    DB_PATH = '../databases/corpus_database.db'
+    # Parse script arguments.
+    parser = setup_argparse()
+    args = parser.parse_args()
+
+    # Read project-wide parameters.
+    with open('./parameters.json') as fp:
+        params_dict = json.load(fp)
+
+    # Get data information.
+    dataset_name = args.dataset_name
+    generated_embeddings_dirpath = params_dict['generated_data']['embedding_output_root_dir']
+    output_model_dirpath = params_dict['generated_data']['trained_model_output_root_dir']
 
     # Set script parameters.
-    num_procs = 10
+    num_procs = params_dict['script_parameters']['num_processes']
 
     # Set model parameters.
-    n = 2
-    emb_dim = 200
-    batch_size = 25000
-    epochs = 1
-
-    num_aspects = 14
+    model_name = args.model
+    emb_dim = params_dict['script_parameters']['embedding_dimension']
+    batch_size = params_dict['training_parameters']['batch_size']
+    
+    window_len = args.ngram_size
+    n = int((window_len - 1) / 2)
+    epochs = args.epochs
+    num_aspects = args.number_of_aspects
     neg_size = batch_size
 
-    print(f'Parameters for training run:\n\tn: {n}\n\temb_dim: {emb_dim}\n\tbatch_size: {batch_size}\n\tepochs: {epochs}\n')
-    print(f'Parameters for the model:\n\tNumber of aspects: {num_aspects}\n\tNegative input size: {neg_size}\n')
-    print('Computing additional parameters for training...')
+    print(f'\nParameters for training run:\n\tn: {n}\n\temb_dim: {emb_dim}\n\tbatch_size: {batch_size}\n\tepochs: {epochs}\n')
+    print(f'\nParameters for the model:\n\tNumber of aspects: {num_aspects}\n\tNegative input size: {neg_size}\n')
 
-    window_len = 2 * n + 1
+    embs_dirpath = f'{generated_embeddings_dirpath}{dataset_name}_n={n}/emb_{dataset_name}-n_{n}/'
+    model_save_path = f'{output_model_dirpath}{dataset_name}_models/{datetime.now()}_{model_name}'
 
-    embs_dirpath = f'./generated_embeddings/restaurantreviews_n={n}/emb_restaurantreviews_n={n}/'
-    db_table_name = f'restaurantreviews_n={n}'
-    model_save_path = f'./TRAINED_MODEL'
+    print(f'Fetching pre-embedded trainng data from path: {embs_dirpath}')
+    print(f'Trained model will be saved to path: {model_save_path}')
 
     num_rows = len([f for f in os.listdir(embs_dirpath) if f.endswith('.npy')])
-    print(f'Found {num_rows} rows in the {db_table_name} table.\n')
+    print(f'\nFound {num_rows} rows in dataset at {embs_dirpath}.')
 
     # Set up data loading for model training.
     emb_data_loader = data_loaders.EmbeddedDataLoader(
